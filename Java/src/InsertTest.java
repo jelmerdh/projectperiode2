749d6835c510a79f604d46af6a1a3fa4c7ad1d01
jelmerdh <45516237+jelmerdh@users.noreply.github.com>
2021-01-23 19:53:46 +0100

    Variabelen aangemaakt/Insert stament verandert

--- Java/src/InsertTest.java
+++ Java/src/InsertTest.java
@@ -1,11 +1,13 @@
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.LinkedList;
+import java.sql.Date;
 
 public class InsertTest {
 
     // AANMAKEN LINKEDLIST
-    private LinkedList<>
+
     private Database db;
 
 
@@ -15,19 +17,36 @@ public class InsertTest {
         test();
     }
 
+    public Date getDate(){
+        long milis = System.currentTimeMillis();
+        java.sql.Date date= new java.sql.Date(milis);
+        return date;
+    }
+
+
+
     public void test(){
         try {
+
+
             System.out.println("Hij begint aan test!");
 
-            String query = "INSERT INTO weatherdata (stn, name, country, latitude, longitude, elevation)"
-                    + "VALUES (?, ?, ?, ?, ?,? )";
+            String query = "INSERT INTO weatherdata (stn, date, time,temp, dewp, stp,slp, visib, wdsp,sndp, frshtt, cldc,  wnddir)"
+                    + "VALUES (?, ?, ?, ?, ?,? , ?, ? ,?, ?, ?, ?, ?)";
             PreparedStatement pstm = db.getMyConn().prepareStatement(query);
             pstm.setInt(1, 999999);
-            pstm.setString(2, "JASPERLAND");
-            pstm.setString(3,"JELMERLAND");
-            pstm.setDouble(4, 50.7);
-            pstm.setDouble(5, 6.9);
-            pstm.setDouble(6, 15.0);
+            pstm.setDate(2, getDate());
+            pstm.setTime(3,);
+            pstm.setFloat(4, temp);
+            pstm.setFloat(5, dewp);
+            pstm.setFloat(6, stp);
+            pstm.setFloat(7, slp);
+            pstm.setFloat(8,visib);
+            pstm.setFloat(9,wdsp);
+            pstm.setFloat(10,sndp);
+            pstm.setString(11,frshtt);
+            pstm.setFloat(12,cldc);
+            pstm.setInt(13,wnddir);
             pstm.execute();
             System.out.println("Query is verstuurd!");
             pstm.close();
