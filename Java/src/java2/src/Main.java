9b3a691f2a8d82248a91153f3ccf7fa78177d6d1
jasper-geertsma <58548553+jasper-geertsma@users.noreply.github.com>
2021-01-26 17:55:19 +0100

    csv writing
    begin gemaakt
    lezen kan niet om een vage filenotfound exeption

---
+++ Java/src/java2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+        public static void main(String[] args) throws Exception    {
+
+            System.out.println("hoi");
+            new CsvWrite();
+            new CsvRead();
+}
+
+    }
+
