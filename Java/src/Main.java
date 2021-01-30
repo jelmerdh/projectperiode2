c8c4ff2ca134f91a7d4279eb26f30314da954eb4
jasper-geertsma <58548553+jasper-geertsma@users.noreply.github.com>
2021-01-30 14:08:25 +0100

    dingen

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -2,9 +2,8 @@ public class Main {
 
         public static void main(String[] args) throws Exception    {
             CsvWrite jack = new CsvWrite();
-            new CsvRead();
             for (int i=0; i<9000; i++) {
-                jack.write(760013, "2021-03-02", "16:01:14", (float)24, (float)5, (float)14,(float)14,(float)35, (float)14,(float)4,"n", (float)15, 3);
+                jack.writeMexico(760013, "2021-03-02", "16:01:14", (float)24, (float)5, (float)14,(float)14,(float)35, (float)14,(float)4,"n", (float)15, 3);
             }
 
         }
