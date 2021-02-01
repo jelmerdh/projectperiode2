12548585d93033a2d313b895f4e18873c85c38c2
jasper-geertsma <58548553+jasper-geertsma@users.noreply.github.com>
2021-02-01 13:32:22 +0100

    Update dashboard.php

--- PHP/dashboard.php
+++ PHP/dashboard.php
@@ -33,10 +33,16 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       </p>
     </figure>
   <?php
+/*
+Ophalen van de csv bestanden en het in een array zetten.
+*/
   $csvPath = "config/TempData.csv";
   $csvData = array_map('str_getcsv', file($csvPath));
   $stnr = array_map('str_getcsv', file("config\stations.csv"));
 
+/*
+Het zoeken naar een station en het result teruggeven.
+*/
   function search($array, $key, $value) {
         $results = array();
         if (is_array($array)) {
@@ -58,7 +64,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         return $results;
     }
     #var_dump($csvData);
-
+  //Array aanmaken voor de hoogste temperaturen
   $hottestStations = array();
 
   function getHighestTemp($file, $array){
@@ -67,12 +73,18 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       $stationnmmr=0;
       $highest = 0;
       $j = 1;
-      while($j < 11){ //Array eerst vullen met, begint bij 1 om de eerste regel van csv te skippen.
+      /*Array eerst vullen met, begint bij 1 om de eerste regel van csv te skippen.
+      Er eerst 10 waardes inzetten zodat de array wordt gevuld.*/
+      while($j < 11){
           $highest = floatval($file[$j][3]);
           $stationnmmr = $file[$j][0];
           $array[$stationnmmr] = $highest;
           $j++;
       }
+      /*Loopen en de temperaturen met elkaar vergelijken. Als de temperatuur hoger is dan de laagste die erin staat dan wordt die vervangen door een nieuwe
+        Hierbij wordt ook gekeken als het stationnummer er al in staat. Staat de station nummer erin en is de temperatuur hogen dan wordt de hogere temperatuur erin gezet.
+        Hij returned de array.
+        */
       for($i=$j;$i<count($file);$i++)
       {
         $highest = floatval($file[$i][3]);
@@ -97,62 +109,68 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       #print("<br>");
       return $array;
   }
+  //Aanmaken array van de hoogste temperaturen gesort van hoog naar laag.
   $hottestArray  = getHighestTemp($csvData, $hottestStations);
 
   function setStationVar($array){
     $tel = 1;
-    #print("$tel<br>");
     $teller = 0;
+    /*
+      Als één van de knoppen wordt ingedrukt zal de variabele tel veranderen naar de positie die in de line graph moet komen.
+      Met de foreach loop ga je door de $hottestarray heen en als de $teller gelijk is aan $tel - 1 dan wordt de station gereturned en dan stopt hij.
+      $tel - 1 wordt gebruikt omdat hij anders niet de correcte positie pakt.
+    */
      if(isset($_POST['setStationVar1'])){
         $tel = 1;
-        #$tel = $_GET['setStationVar1'];
-        #print("$tel<br>");
       }
       else if(isset($_POST['setStationVar2'])){
         $tel = 2;
-        #print("$tel<br>");
       }
       else if(isset($_POST['setStationVar3'])){
         $tel = 3;
-        #print("$tel<br>");
       }
       else if(isset($_POST['setStationVar4'])){
         $tel = 4;
-        #print("$tel<br>");
       }
       else if(isset($_POST['setStationVar5'])){
         $tel = 5;
-      }else if(isset($_POST['setStationVar6'])){
+      }
+      else if(isset($_POST['setStationVar6'])){
         $tel = 6;
-      }else if(isset($_POST['setStationVar7'])){
+      }
+      else if(isset($_POST['setStationVar7'])){
         $tel = 7;
-      }else if(isset($_POST['setStationVar8'])){
+      }
+      else if(isset($_POST['setStationVar8'])){
         $tel = 8;
-      }else if(isset($_POST['setStationVar9'])){
+      }
+      else if(isset($_POST['setStationVar9'])){
         $tel = 9;
-      }else if(isset($_POST['setStationVar10'])){
+      }
+      else if(isset($_POST['setStationVar10'])){
         $tel = 10;
       }
-      #print("$tel<br>");
       foreach ($array as $station => $temp) {
           if($teller == $tel - 1){
             $teststation = $station;
-            #print("$teststation");
+
             return $teststation;
             break;
           }
           else{
             $teller++;
-            #print("$teller<br>");
+
         }
       }
     }
+    /*  Aanmaken van de stationvar */
   $stationvar = setStationVar($hottestArray);
 
-  #$station = getHighestTemp($csvData, $hottestStations);
-  echo "<br>";
-  #var_dump($station);
-
+/*
+ Het aanmaken van een nieuwe array $dataPoints hier wordt gebruikt gemaakt van de search methode die alle data opzoekt die bij dat station hoort.
+ In de foreach loop wordt iedere keer de datum en temperatuur gepushed in de datapoints array. Het javascripts gedeelte maakt gebruik van deze array.
+ y staat voor de temperatuur en label voor de datum.
+*/
     $dataPoints = array();
     $res = search($csvData, '0', $stationvar);
     foreach ($res as $var) {
@@ -175,12 +193,20 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   }
   </script>
   <script>
+  /*
+  Aanmaken van de line chart.
+  Hierbij wordt gebruik gemaakt van de datapoints array die eerder is aangemaakt.
+  JSON_NUMERIC_CHECK encodes numeric strings als numbers.
+  Hierbij is gebruikt gemaakt van canvasjs zie link:
+  source:  https://canvasjs.com/php-charts/line-chart/
+  */
   window.onload = function () {
     CanvasJS.addColorSet("red",
         [//colorSet Array
 
         "#05386B",
         ]);
+
 
   var chart = new CanvasJS.Chart("chartContainer", {
   	animationEnabled: true,
@@ -191,6 +217,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   		text: "Top 10 hottest weather",
       fontColor: "#05386B",
   	},
+    /* Y as voor de temperatuur.
+    */
   	axisY: {
   		title: "Temperature",
       fontColor: "#05386B",
@@ -204,7 +232,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   	}]
   });
 
-
+//Render de line chart
   chart.render();
 
   }
@@ -214,29 +242,29 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
   <?php
-  //$hottestkeys shows the $hottest weather
+
     $hottestkeys  = array_keys($hottestArray);
 
     for ($i=0; $i < count($hottestkeys); $i++) {
       $vez[] = search($stnr, '0', $hottestkeys[$i]);
 
     }
-
+    //this loop only gets the station numbers from the array $csv
     foreach ($vez as $ster) {
       $sarr[] = $ster["0"]["2"];
       $narr[] = $ster["0"]["1"];
     }
+     //the array is cleared from duplicates, it skips the first value (stn) and gets reindexed
     $hotCountry = array_values($sarr);
     $hotCity = array_values($narr);
    ?>
 
   <form action="" method="post">
     <?php
+    //this loop creates boxes with stationnumbers on which you can click on.
       $count = 1;
-      //As long as there is a value in $hottestKeys, there will be another button made.
+      //this if-statement creates a new row of there are 14 columns made.
       for ($i=0; $i < count($hottestkeys) ; $i++) {
-        //If $hotCity already contains a country, then there won't be a nother country added to the button/
-
         if (strpos($hotCity[$i], $hotCountry[$i]) !== false) {
           echo '<button type="submit"  name="setStationVar'.$i.'" class="submit button'.$i.'1"  value="1">'.$count.'. '. $hotCity[$i] .'</button>';
         }
